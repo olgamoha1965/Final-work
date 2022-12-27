4a31bc531efe599a635dd946a1f32b86ce26a0c8
olgamoha1965 <115876778+olgamoha1965@users.noreply.github.com>
2022-12-27 08:40:06 +0300

    Program_Final Task

--- Program.cs
+++ Program.cs
@@ -14,14 +14,18 @@ void ShowArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write(array[i] + " ");
+        Console.Write($"{array[i]}" + " ");
     }
 }
 
-string[] array1 = new string[4] {"Hello", "2", "world", ":-)"};
+//string[] array1 = new string[4] {"Hello", "2", "world", ":-)"};
+//string[] array1 = new string[4] {"1234", "1567", "-2", "computer science"};
+string[] array1 = new string[3] {"Russia", "Denmark", "Kazan"};
 string[] array2 = new string[array1.Length];
 
 CurrentArray(array1, array2);
+Console.Write("[");
 ShowArray(array1);
-Console.Write(" -> ");
+Console.Write("] -> [");
 ShowArray(array2);
+Console.Write("]");
