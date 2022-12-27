e5149bb5d1743790d043d6daa03d1877eea7d9b9
olgamoha1965 <115876778+olgamoha1965@users.noreply.github.com>
2022-12-27 07:40:54 +0300

    Program_Final Task

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿void CurrentArray(string[] array1, string[] array2)
+{
+    int k = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[k] = array1[i];
+        k++;
+        }
+    }
+}
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+string[] array1 = new string[4] {"Hello", "2", "world", ":-)"};
+string[] array2 = new string[array1.Length];
+
+CurrentArray(array1, array2);
+ShowArray(array1);
+Console.Write(" -> ");
+ShowArray(array2);
